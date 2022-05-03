e669995740c426c3534178fb9b0fdcd38025dcf5
Trychangelife <zkonstantinov@bk.ru>
2022-05-03 21:51:39 +0700

    Update index.ts

--- BloggerPost/src/index.ts
+++ BloggerPost/src/index.ts
@@ -7,11 +7,36 @@ const port = 5000
 app.use(cors())
 app.use(bodyParser.json())
 
-
+let bloggers = [
+  { id: 1, name: 'Alex', youtubeUrl: 'Alex_TV' },
+  { id: 2, name: 'Bob', youtubeUrl: 'Bob_TV' },
+  { id: 3, name: 'Jon', youtubeUrl: 'Jon_TV' },
+  { id: 4, name: 'Trevis', youtubeUrl: 'Trevis_TV' },
+  { id: 5, name: 'Michael', youtubeUrl: 'Michael_TV' },
+]
 
 app.get('/', (req: Request, res: Response ) => {
     res.send('Hello: World!')
 })
+
+app.get('/bloggers', (req: Request, res: Response) => {
+  res.status(200).send(bloggers)
+})
+app.get('/bloggers/:id', (req: Request, res: Response) => {
+  const id = +req.params.id
+  const blogger = bloggers.find((b) => {
+    if (b.id === id) return true;
+    else return false;
+  })
+
+  if (blogger !== undefined) {
+    res.send(blogger)
+  }
+  else {
+    res.send(404)
+  }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
