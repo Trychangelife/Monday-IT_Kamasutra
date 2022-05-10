846d021ca88bba8d2f63ba5d54f82e19881ed01c
Trychangelife <zkonstantinov@bk.ru>
2022-05-10 16:06:33 +0700

    try connect mongoDB

--- BloggerPost/src/index.ts
+++ BloggerPost/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import { bloggersRouter } from './routes/bloggers-router';
 import { postRouter } from './routes/posts-router';
 import { booksRouter } from './routes/books-routes';
+import { runDb } from './repositories/db';
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -14,6 +15,15 @@ app.use('/bloggers', bloggersRouter)
 app.use('/posts', postRouter)
 app.use('/books', booksRouter)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const startApp = async () => {
+  await runDb()
+  app.listen(port, () => {
+    console.log(`Server listening on post: ${port}`)
+  })
+}
+
+startApp()
---
+++ BloggerPost/src/repositories/db.ts
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+const mongoUri = process.env.mongoURI || "mongodb://0.0.0.0:27017";
+
+export const client = new MongoClient(mongoUri);
+
+export async function runDb() {
+    try {
+        //Connect the client to the server
+        await client.connect();
+        // Establish (Создавать) and verify connection
+        await client.db("bloggers").command({ ping: 1});
+        console.log("Connected successfully to mongo server");
+    }
+    catch {
+        console.log("can't connect to DB")
+        //Ensures that the client will close when you finish/error
+        await client.close()
+    }
+}
